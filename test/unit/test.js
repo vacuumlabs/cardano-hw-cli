7718acc4a3c8988f622ed81862e76a22ac8f0303
Peter Benc <peter.benc1@gmail.com>
2020-10-07 01:12:31 +0200

    Refactor txAux tests

--- test/unit/test.js
+++
@@ -1,7 +0,0 @@
-const assert = require('assert')
-
-describe('Test demo', () => {
-  it('Should serve as a test demo', () => {
-    assert.strictEqual(true, true)
-  })
-})
--- test/unit/transaction/txAux.js
+++ test/unit/transaction/txAux.js
@@ -2,37 +2,20 @@ const assert = require('assert')
 const { TxAux } = require('../../../src/transaction/txBuilder')
 const { transactions } = require('./txs')
 
-function testTxHashAndParsing(transaction) {
+function testTxHashAndParsing(tx) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { getId, unsignedTxDecoded, ...parsedTx } = TxAux(transaction.unsignedTx)
-  const txHash = getId()
+  const { getId, unsignedTxDecoded, ...parsedTx } = TxAux(tx.unsignedCborHex)
+  const txHashHex = getId()
   it('Should parse tx', () => {
-    assert.deepStrictEqual(parsedTx, transaction.txParsed)
+    assert.deepStrictEqual(parsedTx, tx.parsed)
   })
-  it('Should get correct transacttransactionion hash', () => {
-    assert.deepStrictEqual(txHash, transaction.txHashHex)
+  it('Should get correct transaction hash', () => {
+    assert.deepStrictEqual(txHashHex, tx.hashHex)
   })
 }
 
 describe('Transaction parsing', () => {
-  describe('Tx with delegation certificate', () => {
-    const transaction = transactions.withDelegationCertificate
-    testTxHashAndParsing(transaction)
-  })
-  describe('Tx with deregistration certificate', () => {
-    const transaction = transactions.withDeregistrationCertificate
-    testTxHashAndParsing(transaction)
-  })
-  describe('Tx with staking key registration certificate', () => {
-    const transaction = transactions.withRegistrationCertificate
-    testTxHashAndParsing(transaction)
-  })
-  describe('Tx with withdrawal', () => {
-    const transaction = transactions.withWithdrawal
-    testTxHashAndParsing(transaction)
-  })
-  describe('Tx with meta data', () => {
-    const transaction = transactions.withMetaData
-    testTxHashAndParsing(transaction)
-  })
+  Object.entries(transactions).forEach(([txType, tx]) => describe(
+    txType, () => testTxHashAndParsing(tx),
+  ))
 })
