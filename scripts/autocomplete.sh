6214edf908a3ebb9473629c24b2da6874dea0234
David Tran Duc <david.tran.duc@vacuumlabs.com>
2021-04-09 16:38:03 +0200

    Update autocomplete.sh script

--- scripts/autocomplete.sh
+++ scripts/autocomplete.sh
@@ -2,7 +2,7 @@ _cardano_hw_cli_completions()
 {
     case $COMP_CWORD in
         1)
-            COMPREPLY=( $(compgen -W "device version address key transaction" "${COMP_WORDS[1]}") )
+            COMPREPLY=( $(compgen -W "device version address key transaction node" "${COMP_WORDS[1]}") )
             ;;
         2)
             if [ "${COMP_WORDS[1]}" = "address" ]; then
@@ -17,9 +17,12 @@ _cardano_hw_cli_completions()
             if [ "${COMP_WORDS[1]}" = "device" ]; then
                 COMPREPLY=( $(compgen -W "version" "${COMP_WORDS[2]}") )
             fi
+            if [ "${COMP_WORDS[1]}" = "node" ]; then
+                COMPREPLY=( $(compgen -W "key-gen issue-op-cert" "${COMP_WORDS[2]}") )
+            fi
             ;;
         *)
-            if [ "${COMP_WORDS[2]}" = "key-gen" ]; then
+            if [ "${COMP_WORDS[1]}" = "address" ] && [ "${COMP_WORDS[2]}" = "key-gen" ]; then
                 COMPREPLY=( $(compgen -W "--path --hw-signing-file --verification-key-file" -- "${COMP_WORDS[-1]}") )
             fi
             if [ "${COMP_WORDS[2]}" = "show" ]; then
@@ -28,11 +31,14 @@ _cardano_hw_cli_completions()
             if [ "${COMP_WORDS[2]}" = "verification-key" ]; then
                 COMPREPLY=( $(compgen -W "--hw-signing-file --verification-key-file" -- "${COMP_WORDS[-1]}") )
             fi
-            if [ "${COMP_WORDS[2]}" = "sign" ]; then
+            if [ "${COMP_WORDS[2]}" = "sign" ] || [ "${COMP_WORDS[2]}" = "witness" ]; then
                 COMPREPLY=( $(compgen -W "--mainnet --testnet-magic --tx-body-file --hw-signing-file --change-output-key-file --out-file" -- "${COMP_WORDS[-1]}") )
             fi
-            if [ "${COMP_WORDS[2]}" = "witness" ]; then
-                COMPREPLY=( $(compgen -W "--mainnet --testnet-magic --tx-body-file --hw-signing-file --change-output-key-file --out-file" -- "${COMP_WORDS[-1]}") )
+            if [ "${COMP_WORDS[1]}" = "node" ] && [ "${COMP_WORDS[2]}" = "key-gen" ]; then
+                COMPREPLY=( $(compgen -W "--path --hw-signing-file --cold-verification-key-file --operational-certificate-issue-counter-file" -- "${COMP_WORDS[-1]}") )
+            fi
+            if [ "${COMP_WORDS[2]}" = "issue-op-cert" ]; then
+                COMPREPLY=( $(compgen -W "--kes-verification-key-file --kes-period --operational-certificate-issue-counter --hw-signing-file --out-file" -- "${COMP_WORDS[-1]}") )
             fi
             ;;
     esac
