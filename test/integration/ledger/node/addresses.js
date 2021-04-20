a045297c9bef4a43a6abf69ca040dcc45a0bd04f
David Tran Duc <david.tran.duc@vacuumlabs.com>
2021-04-20 16:08:43 +0200

    Add voting registration ledger integration test

---
+++ test/integration/ledger/node/addresses.js
@@ -0,0 +1,14 @@
+/* eslint-disable max-len */
+
+const addresses = {
+  mainnet: {
+    address0: 'addr1qy2vzmtlgvjrhkq50rngh8d482zj3l20kyrc6kx4ffl3zfqayfawlf9hwv2fzuygt2km5v92kvf8e3s3mk7ynxw77cwqf7zhh2',
+  },
+  testnet: {
+    address0: 'addr_test1qq2vzmtlgvjrhkq50rngh8d482zj3l20kyrc6kx4ffl3zfqayfawlf9hwv2fzuygt2km5v92kvf8e3s3mk7ynxw77cwq2glhm4',
+  },
+}
+
+module.exports = {
+  addresses,
+}
---
+++ test/integration/ledger/node/votingRegistration.js
@@ -0,0 +1,47 @@
+/* eslint-disable max-len */
+const assert = require('assert')
+const { LedgerCryptoProvider } = require('../../../../src/crypto-providers/ledgerCryptoProvider')
+
+const { NETWORKS } = require('../../../../src/constants')
+const { signingFiles } = require('./signingFiles')
+const { addresses } = require('./addresses')
+
+const votingRegistrations = {
+  votingRegistration1: {
+    network: 'TESTNET',
+    auxiliarySigningFiles: [signingFiles.payment0, signingFiles.stake0],
+    hwStakeSigningFile: signingFiles.stake0,
+    paymentAddressBech32: addresses.testnet.address0,
+    votePublicKeyHex: '3b40265111d8bb3c3c608d95b3a0bf83461ace32d79336579a1939b3aad1c0b7',
+    nonce: 165564,
+    signedVotingRegistrationMetaDataHex: 'a219ef64a40158203b40265111d8bb3c3c608d95b3a0bf83461ace32d79336579a1939b3aad1c0b702582066610efd336e1137c525937b76511fbcf2a0e6bcf0d340a67bcb39bc870d85e80358390014c16d7f43243bd81478e68b9db53a8528fd4fb1078d58d54a7f11241d227aefa4b773149170885aadba30aab3127cc611ddbc4999def61c041a000286bc19ef65a10158408a8cca0fbc26626c65eaf28608da21527c70c8f6136e3eb5166e8d665ef4f9f55e0b8063393d63bfa0104a1ac737a10c1c41a93a68d101a4cb0b405ec5bb6707',
+  },
+}
+
+async function testVotingRegistrationMetaDataSigning(cryptoProvider, votingRegistration) {
+  const { signedVotingRegistrationMetaDataHex, ...args } = votingRegistration
+
+  assert.deepStrictEqual(
+    await cryptoProvider.signVotingRegistrationMetaData(
+      args.auxiliarySigningFiles,
+      args.hwStakeSigningFile,
+      args.paymentAddressBech32,
+      args.votePublicKeyHex,
+      NETWORKS[args.network],
+      args.nonce,
+    ),
+    signedVotingRegistrationMetaDataHex,
+  )
+}
+
+describe('Ledger sign voting registration metadata', () => {
+  let cryptoProvider
+  before(async () => {
+    cryptoProvider = await LedgerCryptoProvider()
+  })
+  const votingRegistrationsToSign = Object.entries(votingRegistrations)
+
+  votingRegistrationsToSign.forEach(([votingRegistrationTestName, votingRegistration]) => it(
+    `Should sign voting registration ${votingRegistrationTestName}`, async () => testVotingRegistrationMetaDataSigning(cryptoProvider, votingRegistration),
+  ).timeout(100000))
+})
