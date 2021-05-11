a0749e135b3130755e45e6afcc0db911eb2278bb
David Tran Duc <david.tran.duc@vacuumlabs.com>
2021-05-11 09:21:07 +0200

    Add mainnet tests for catalyst voting registration

--- test/integration/ledger/node/votingRegistration.js
+++ test/integration/ledger/node/votingRegistration.js
@@ -7,7 +7,7 @@ const { signingFiles } = require('./signingFiles')
 const { addresses } = require('./addresses')
 
 const votingRegistrations = {
-  withBaseAddress0: {
+  withTestnetBaseAddress0: {
     network: 'TESTNET',
     auxiliarySigningFiles: [signingFiles.payment0, signingFiles.stake0],
     hwStakeSigningFile: signingFiles.stake0,
@@ -16,7 +16,16 @@ const votingRegistrations = {
     nonce: 165564,
     signedVotingRegistrationMetaDataHex: 'a219ef64a40158203b40265111d8bb3c3c608d95b3a0bf83461ace32d79336579a1939b3aad1c0b702582066610efd336e1137c525937b76511fbcf2a0e6bcf0d340a67bcb39bc870d85e80358390014c16d7f43243bd81478e68b9db53a8528fd4fb1078d58d54a7f11241d227aefa4b773149170885aadba30aab3127cc611ddbc4999def61c041a000286bc19ef65a10158408a8cca0fbc26626c65eaf28608da21527c70c8f6136e3eb5166e8d665ef4f9f55e0b8063393d63bfa0104a1ac737a10c1c41a93a68d101a4cb0b405ec5bb6707',
   },
-  withRewardAddress0: {
+  withMainnetBaseAddress0: {
+    network: 'MAINNET',
+    auxiliarySigningFiles: [signingFiles.payment0, signingFiles.stake0],
+    hwStakeSigningFile: signingFiles.stake0,
+    rewardAddressBech32: addresses.mainnet.base0,
+    votePublicKeyHex: '3b40265111d8bb3c3c608d95b3a0bf83461ace32d79336579a1939b3aad1c0b7',
+    nonce: 165564,
+    signedVotingRegistrationMetaDataHex: 'a219ef64a40158203b40265111d8bb3c3c608d95b3a0bf83461ace32d79336579a1939b3aad1c0b702582066610efd336e1137c525937b76511fbcf2a0e6bcf0d340a67bcb39bc870d85e80358390114c16d7f43243bd81478e68b9db53a8528fd4fb1078d58d54a7f11241d227aefa4b773149170885aadba30aab3127cc611ddbc4999def61c041a000286bc19ef65a101584031e0da1b744776cec0f1dd9a4668b0a92dd61d8ca62fb1f923f469ff8f2c3c30034ad9e4d23a22ee92984efb12dbf5bc1ec5bdb8aa9073fb03f7e994cb061004',
+  },
+  withTestnetRewardAddress0: {
     network: 'TESTNET',
     auxiliarySigningFiles: [signingFiles.payment0, signingFiles.stake0],
     hwStakeSigningFile: signingFiles.stake0,
@@ -24,6 +33,15 @@ const votingRegistrations = {
     votePublicKeyHex: '3b40265111d8bb3c3c608d95b3a0bf83461ace32d79336579a1939b3aad1c0b7',
     nonce: 165564,
     signedVotingRegistrationMetaDataHex: 'a219ef64a40158203b40265111d8bb3c3c608d95b3a0bf83461ace32d79336579a1939b3aad1c0b702582066610efd336e1137c525937b76511fbcf2a0e6bcf0d340a67bcb39bc870d85e803581de01d227aefa4b773149170885aadba30aab3127cc611ddbc4999def61c041a000286bc19ef65a1015840d4401401a6f447c3e906c4dde2193f19a7c9085040c73f7941bb4cda212ada78a9c21f0b6e1afc54a34e17ecfc9e31bc46bb7836ffddf94b985e78f490159b06',
+  },
+  withMainnetRewardAddress0: {
+    network: 'MAINNET',
+    auxiliarySigningFiles: [signingFiles.payment0, signingFiles.stake0],
+    hwStakeSigningFile: signingFiles.stake0,
+    rewardAddressBech32: addresses.mainnet.reward0,
+    votePublicKeyHex: '3b40265111d8bb3c3c608d95b3a0bf83461ace32d79336579a1939b3aad1c0b7',
+    nonce: 165564,
+    signedVotingRegistrationMetaDataHex: 'a219ef64a40158203b40265111d8bb3c3c608d95b3a0bf83461ace32d79336579a1939b3aad1c0b702582066610efd336e1137c525937b76511fbcf2a0e6bcf0d340a67bcb39bc870d85e803581de11d227aefa4b773149170885aadba30aab3127cc611ddbc4999def61c041a000286bc19ef65a1015840269e36e855a7200606be820f369f83ee4e942594900ca20c8a28ef4a21ed77fd17bba7236096a7873ef026cc912708d9014a436f074fe300e86a9c1e4da52909',
   },
 }
 
--- test/integration/trezor/node/votingRegistration.js
+++ test/integration/trezor/node/votingRegistration.js
@@ -7,7 +7,7 @@ const { signingFiles } = require('./signingFiles')
 const { addresses } = require('./addresses')
 
 const votingRegistrations = {
-  withBaseAddress0: {
+  withTestnetBaseAddress0: {
     network: 'TESTNET',
     auxiliarySigningFiles: [signingFiles.payment0, signingFiles.stake0],
     hwStakeSigningFile: signingFiles.stake0,
@@ -16,7 +16,16 @@ const votingRegistrations = {
     nonce: 165564,
     signedVotingRegistrationMetaDataHex: 'a219ef64a40158203b40265111d8bb3c3c608d95b3a0bf83461ace32d79336579a1939b3aad1c0b7025820bc65be1b0b9d7531778a1317c2aa6de936963c3f9ac7d5ee9e9eda25e0c97c5e0358390080f9e2c88e6c817008f3a812ed889b4a4da8e0bd103f86e7335422aa122a946b9ad3d2ddf029d3a828f0468aece76895f15c9efbd69b4277041a000286bc19ef65a1015840b58d8a168389bf29b654ff8c6b82b1f9d9d32f6f51f3a7ca2e41cb8d9098e2e8dd5da3d9641125318dac237d27e5136a6c5ad644e38dcd13383f783cd38b6c0a',
   },
-  withRewardAddress0: {
+  withMainnetBaseAddress0: {
+    network: 'MAINNET',
+    auxiliarySigningFiles: [signingFiles.payment0, signingFiles.stake0],
+    hwStakeSigningFile: signingFiles.stake0,
+    rewardAddressBech32: addresses.mainnet.base0,
+    votePublicKeyHex: '3b40265111d8bb3c3c608d95b3a0bf83461ace32d79336579a1939b3aad1c0b7',
+    nonce: 165564,
+    signedVotingRegistrationMetaDataHex: 'a219ef64a40158203b40265111d8bb3c3c608d95b3a0bf83461ace32d79336579a1939b3aad1c0b7025820bc65be1b0b9d7531778a1317c2aa6de936963c3f9ac7d5ee9e9eda25e0c97c5e0358390180f9e2c88e6c817008f3a812ed889b4a4da8e0bd103f86e7335422aa122a946b9ad3d2ddf029d3a828f0468aece76895f15c9efbd69b4277041a000286bc19ef65a10158408ecccc5267b9e3cb36bf4964aa8e7656ca2ad0f6b418b090da968fb1b6774e09d07e4f747aa471388be1c4f0c28e3d864a4f67d68e9ec6280b5b891b6123bf00',
+  },
+  withTestnetRewardAddress0: {
     network: 'TESTNET',
     auxiliarySigningFiles: [signingFiles.payment0, signingFiles.stake0],
     hwStakeSigningFile: signingFiles.stake0,
@@ -24,6 +33,15 @@ const votingRegistrations = {
     votePublicKeyHex: '3b40265111d8bb3c3c608d95b3a0bf83461ace32d79336579a1939b3aad1c0b7',
     nonce: 165564,
     signedVotingRegistrationMetaDataHex: 'a219ef64a40158203b40265111d8bb3c3c608d95b3a0bf83461ace32d79336579a1939b3aad1c0b7025820bc65be1b0b9d7531778a1317c2aa6de936963c3f9ac7d5ee9e9eda25e0c97c5e03581de0122a946b9ad3d2ddf029d3a828f0468aece76895f15c9efbd69b4277041a000286bc19ef65a10158404e84dc543384bd7bb7f9e6008e7fb50fc6f786964a884661f696c6877c11a90d807616033cca7648e105e9e3d6d9db94181d3636c62bc106f52262f6b1284b0d',
+  },
+  withMainnetRewardAddress0: {
+    network: 'MAINNET',
+    auxiliarySigningFiles: [signingFiles.payment0, signingFiles.stake0],
+    hwStakeSigningFile: signingFiles.stake0,
+    rewardAddressBech32: addresses.mainnet.reward0,
+    votePublicKeyHex: '3b40265111d8bb3c3c608d95b3a0bf83461ace32d79336579a1939b3aad1c0b7',
+    nonce: 165564,
+    signedVotingRegistrationMetaDataHex: 'a219ef64a40158203b40265111d8bb3c3c608d95b3a0bf83461ace32d79336579a1939b3aad1c0b7025820bc65be1b0b9d7531778a1317c2aa6de936963c3f9ac7d5ee9e9eda25e0c97c5e03581de1122a946b9ad3d2ddf029d3a828f0468aece76895f15c9efbd69b4277041a000286bc19ef65a10158408455bcad60398f8c795408ac05cdb470f67ef6c8f9b4bdfb2700e01fd61bc09161ad36e64347d8cca52f72ad8d1e7f2810322e402ed46c5a8318783dd8c76808',
   },
 }
 
