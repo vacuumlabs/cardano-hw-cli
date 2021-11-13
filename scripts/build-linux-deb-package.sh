c4325ce250faba3bbb22f101157655f9322963a3
David Misiak <david.misiak@vacuumlabs.com>
2021-11-13 17:24:15 +0100

    Update node, mocha, add cardano-hw-interop-lib

--- scripts/build-linux-deb-package.sh
+++ scripts/build-linux-deb-package.sh
@@ -26,6 +26,7 @@ mkdir ./build/linux/deb/${PACKAGE}_${VERSION}/usr/share/cardano-hw-cli
 mkdir ./build/linux/deb/${PACKAGE}_${VERSION}/DEBIAN
 
 # Build executable
+# TODO update version when Nexe supports 12.19.1 (or higher) https://github.com/nexe/nexe/issues/821
 yarn nexe ./dist/index.js -o ./build/linux/deb/cardano-hw-cli -t linux-x64-12.16.2
 
 # Copy files to package structure
