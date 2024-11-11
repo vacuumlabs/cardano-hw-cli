412501ad01eb822db9d63edccd2d9b30cf9f5dee
ZhenQian <zhenqian@keyst.one>
2024-11-11 10:38:35 +0800

    fix: fix build shell and usb bug

--- scripts/build-common.sh
+++ scripts/build-common.sh
@@ -7,6 +7,9 @@ yarn clean
 yarn install
 yarn build-js
 
-# Update commit hash in package.json
-COMMIT_HASH=$(git rev-parse HEAD)
-sed -i '/"commit":.*,/d' package.json && sed -i '4 i \  "commit": "'${COMMIT_HASH}'",\' package.json
+# print commit hash
+echo "Commit hash: ${COMMIT_HASH}"
+
+sed -i '' '/"commit":.*,/d' package.json && sed -i '' '4 i \
+  "commit": "'${COMMIT_HASH}'",\
+' package.json
