d8ae90b2f8afc5f3c08c1d970fc9a02ca2157330
ZhenQian <zhenqian@keyst.one>
2024-12-23 09:36:53 +0800

    fix: remove print commit hash from outside

--- scripts/build-common.sh
+++ scripts/build-common.sh
@@ -7,9 +7,6 @@ yarn clean
 yarn install
 yarn build-js
 
-# print commit hash
-echo "Commit hash: ${COMMIT_HASH}"
-
-sed -i '' '/"commit":.*,/d' package.json && sed -i '' '4 i \
-  "commit": "'${COMMIT_HASH}'",\
-' package.json
+# Update commit hash in package.json
+COMMIT_HASH=$(git rev-parse HEAD)
+sed -i '/"commit":.*,/d' package.json && sed -i '4 i \  "commit": "'${COMMIT_HASH}'",\' package.json
