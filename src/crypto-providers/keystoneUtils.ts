3bea9731aff183d42d8304f2aebe45702a7f7f52
ZhenQian <zhenqian@keyst.one>
2025-03-17 13:31:12 +0800

    fix: fix dep

--- src/crypto-providers/keystoneUtils.ts
+++ src/crypto-providers/keystoneUtils.ts
@@ -16,8 +16,7 @@ import {UR, UREncoder, URDecoder} from '@ngraveio/bc-ur'
 import {Actions, TransportHID} from '@keystonehq/hw-transport-usb'
 import {throwTransportError, Status} from '@keystonehq/hw-transport-error'
 import CardanoSerializationLib from '@emurgo/cardano-serialization-lib-nodejs'
-// @ts-expect-error cardano-crypto.js is not typed
-import bech32 from 'cardano-crypto.js'
+import {bech32} from 'bech32'
 import KeystoneSDK, {
   CardanoCatalystRequestProps,
   CardanoSignCip8MessageData,
