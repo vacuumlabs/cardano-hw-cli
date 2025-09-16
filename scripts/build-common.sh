1744d2b6d38fe630f8f7ab17c0bfd7b697c36871
gabrielkerekes <gabriel.kerekes@vacuumlabs.com>
2025-09-16 14:15:32 +0200

    fix: building on macos machines (might break others though)

--- scripts/build-common.sh
+++ scripts/build-common.sh
@@ -9,4 +9,6 @@ yarn build-js
 
 # Update commit hash in package.json
 COMMIT_HASH=$(git rev-parse HEAD)
-sed -i '/"commit":.*,/d' package.json && sed -i '4 i \  "commit": "'${COMMIT_HASH}'",\' package.json
+sed -i '' '/"commit":.*,/d' package.json && sed -i '' '4 i \
+  "commit": "'${COMMIT_HASH}'",\
+' package.json
