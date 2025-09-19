012dca549fdaf6da29562ab2045dc4b52a73893c
Gabriel Kerekes <gabriel.kerekes@vacuumlabs.com>
2025-09-19 12:06:28 +0200

    chore: remove setting commit hash in package.json as we don't use it anyways

--- scripts/build-common.sh
+++ scripts/build-common.sh
@@ -7,9 +7,3 @@ yarn clean
 # --ignore-engines needed because of old node version, can be removed after node update
 yarn install --ignore-engines
 yarn build-js
-
-# Update commit hash in package.json
-COMMIT_HASH=$(git rev-parse HEAD)
-sed -i '' '/"commit":.*,/d' package.json && sed -i '' '4 i \
-  "commit": "'${COMMIT_HASH}'",\
-' package.json
